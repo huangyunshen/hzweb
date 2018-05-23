2112cfa1174adc30c50588075ea7b3b2a133277a
davos <694150121@qq.com>
2018-05-23 19:43:50 +0800

    列表隐藏bug

--- src/components/indexRouter/mainScreen.vue
+++ src/components/indexRouter/mainScreen.vue
@@ -1,39 +1,41 @@
 <template>
     <div class="mainScreen">
-        <nav>
-            <ul class="no-select-text">
-                <router-link tag="li" :to="{name:'accountInfo'}" @click.native="selectAnItem('1')"
-                             :class="{active:itemSelected==='1'}">
-                    <i class="nav-icon1" :class="{'nav-icon1-active':itemSelected==='1'}"></i>
-                    <span>账户信息</span>
-                </router-link>
-                <router-link tag="li" :to="{name:'tradeRecord'}" @click.native="selectAnItem('2')"
-                             :class="{active:itemSelected==='2'}">
-                    <i class="nav-icon2" :class="{'nav-icon2-active':itemSelected==='2'}"></i>
-                    <span>交易记录</span>
-                </router-link>
-                <router-link tag="li" :to="{name:'transaction'}" @click.native="selectAnItem('3')"
-                             :class="{active:itemSelected==='3'}">
-                    <i class="nav-icon3" :class="{'nav-icon3-active':itemSelected==='3'}"></i>
-                    <span>对外转账</span>
-                </router-link>
-                <router-link tag="li" :to="{name:'applications'}" @click.native="selectAnItem('4')"
-                             :class="{active:itemSelected==='4'}">
-                    <i class="nav-icon4" :class="{'nav-icon4-active':itemSelected==='4'}"></i>
-                    <span>应用市场</span>
-                </router-link>
-                <router-link tag="li" :to="{name:'createApp'}" @click.native="selectAnItem('5')"
-                             :class="{active:itemSelected==='5'}">
-                    <i class="nav-icon5" :class="{'nav-icon5-active':itemSelected==='5'}"></i>
-                    <span>创建应用</span>
-                </router-link>
-                <router-link tag="li" :to="{name:'myApps'}" @click.native="selectAnItem('6')"
-                             :class="{active:itemSelected==='6'}">
-                    <i class="nav-icon6" :class="{'nav-icon6-active':itemSelected==='6'}"></i>
-                    <span>我的应用</span>
-                </router-link>
-            </ul>
-        </nav>
+        <div class="left-nav">
+            <nav>
+                <ul class="no-select-text">
+                    <router-link tag="li" :to="{name:'accountInfo'}" @click.native="selectAnItem('1')"
+                                 :class="{active:itemSelected==='1'}">
+                        <i class="nav-icon1" :class="{'nav-icon1-active':itemSelected==='1'}"></i>
+                        <span>账户信息</span>
+                    </router-link>
+                    <router-link tag="li" :to="{name:'tradeRecord'}" @click.native="selectAnItem('2')"
+                                 :class="{active:itemSelected==='2'}">
+                        <i class="nav-icon2" :class="{'nav-icon2-active':itemSelected==='2'}"></i>
+                        <span>交易记录</span>
+                    </router-link>
+                    <router-link tag="li" :to="{name:'transaction'}" @click.native="selectAnItem('3')"
+                                 :class="{active:itemSelected==='3'}">
+                        <i class="nav-icon3" :class="{'nav-icon3-active':itemSelected==='3'}"></i>
+                        <span>对外转账</span>
+                    </router-link>
+                    <router-link tag="li" :to="{name:'applications'}" @click.native="selectAnItem('4')"
+                                 :class="{active:itemSelected==='4'}">
+                        <i class="nav-icon4" :class="{'nav-icon4-active':itemSelected==='4'}"></i>
+                        <span>应用市场</span>
+                    </router-link>
+                    <router-link tag="li" :to="{name:'createApp'}" @click.native="selectAnItem('5')"
+                                 :class="{active:itemSelected==='5'}">
+                        <i class="nav-icon5" :class="{'nav-icon5-active':itemSelected==='5'}"></i>
+                        <span>创建应用</span>
+                    </router-link>
+                    <router-link tag="li" :to="{name:'myApps'}" @click.native="selectAnItem('6')"
+                                 :class="{active:itemSelected==='6'}">
+                        <i class="nav-icon6" :class="{'nav-icon5-active':itemSelected==='6'}"></i>
+                        <span>我的应用</span>
+                    </router-link>
+                </ul>
+            </nav>
+        </div>
 
         <div class="container">
             <header class="header">
@@ -53,7 +55,7 @@
 </template>
 
 <script>
-    import language from '../language/language';
+    import language from '../language/language'
 
     export default {
         name: "main-screen",
@@ -67,9 +69,9 @@
         },
         methods: {
             selectAnItem(index) {
-                this.itemSelected = index;
+                this.itemSelected = index
             },
-            exit(){
+            exit() {
                 this.$funs.linkToFirstScreenRep()
             }
         },
@@ -77,24 +79,24 @@
             switch (this.$route.name) {
                 case 'accountInfo':
                     this.itemSelected = '1'
-                    break;
+                    break
                 case 'tradeRecord':
                     this.itemSelected = '2'
-                    break;
+                    break
                 case 'transaction':
                     this.itemSelected = '3'
-                    break;
+                    break
                 case 'applications':
                     this.itemSelected = '4'
-                    break;
+                    break
                 case 'createApp':
                     this.itemSelected = '5'
-                    break;
+                    break
                 case 'myApps':
                     this.itemSelected = '6'
-                    break;
+                    break
                 default:
-                    break;
+                    break
             }
         }
     }
@@ -108,97 +110,105 @@
         overflow: hidden;
 
         /**  侧边栏   **/
-        nav {
+        .left-nav{
             width: 400px;
-            height: $height;
             float: left;
-            margin-top: 160px;
-            padding: 30px;
-            border-top-style: solid;
-            border-top-width: 2px;
-            border-image-source: linear-gradient(-85deg,
-                    #3410f7 0%,
-                    #711bdc 59%,
-                    #ad25c0 100%);
-            border-image-slice: 1;
+            padding-top: 150px;
+            height: $height;
             -webkit-box-sizing: border-box;
             -moz-box-sizing: border-box;
             box-sizing: border-box;
-            background-color: rgba(10, 7, 85, 0.8);
 
-            ul {
-                li {
-                    height: 90px;
-                    background: url("../../assets/images/mainScreen/btn.png") no-repeat;
-                    margin-top: 30px;
-                    text-align: center;
-                    line-height: 90px;
-                    font-size: 24px;
-                    color: #8abdec;
+            nav {
+                height: $height;
+                padding: 30px;
+                overflow-y: auto;
+                border-top-style: solid;
+                border-top-width: 2px;
+                border-image-source: linear-gradient(-85deg,
+                        #3410f7 0%,
+                        #711bdc 59%,
+                        #ad25c0 100%);
+                border-image-slice: 1;
+                -webkit-box-sizing: border-box;
+                -moz-box-sizing: border-box;
+                box-sizing: border-box;
+                background-color: rgba(10, 7, 85, 0.8);
 
-                    span {
-                        cursor: default;
+                ul {
+                    li {
+                        height: 90px;
+                        background: url("../../assets/images/mainScreen/btn.png") no-repeat;
+                        margin-top: 30px;
+                        text-align: center;
+                        line-height: 90px;
+                        font-size: 24px;
+                        color: #8abdec;
+
+                        span {
+                            cursor: default;
+                        }
+                        i {
+                            display: inline-block;
+                            width: 30px;
+                            height: 30px;
+                            margin-right: 5px;
+                            vertical-align: text-top;
+                        }
+                        .nav-icon1 {
+                            background: url("../../assets/images/mainScreen/icon_zhxx.png") no-repeat;
+                        }
+                        .nav-icon1-active {
+                            width: 34px;
+                            height: 35px;
+                            background: url("../../assets/images/mainScreen/icon_zhxx_xz.png") no-repeat;
+                        }
+                        .nav-icon2 {
+                            background: url("../../assets/images/mainScreen/icon_jyjl.png") no-repeat;
+                        }
+                        .nav-icon2-active {
+                            width: 34px;
+                            height: 35px;
+                            background: url("../../assets/images/mainScreen/icon_zhxx_xz.png") no-repeat;
+                        }
+                        .nav-icon3 {
+                            background: url("../../assets/images/mainScreen/icon_dwzz.png") no-repeat;
+                        }
+                        .nav-icon3-active {
+                            width: 34px;
+                            height: 35px;
+                            background: url("../../assets/images/mainScreen/icon_zhxx_xz.png") no-repeat;
+                        }
+                        .nav-icon4 {
+                            background: url("../../assets/images/mainScreen/icon_yysc.png") no-repeat;
+                        }
+                        .nav-icon4-active {
+                            width: 34px;
+                            height: 35px;
+                            background: url("../../assets/images/mainScreen/icon_zhxx_xz.png") no-repeat;
+                        }
+                        .nav-icon5 {
+                            background: url("../../assets/images/mainScreen/icon_cjyy.png") no-repeat;
+                        }
+                        .nav-icon5-active {
+                            width: 34px;
+                            height: 35px;
+                            background: url("../../assets/images/mainScreen/icon_zhxx_xz.png") no-repeat;
+                        }
+                        .nav-icon6 {
+                            background: url("../../assets/images/mainScreen/icon_cjyy.png") no-repeat;
+                        }
+                        .nav-icon6-active {
+                            width: 34px;
+                            height: 35px;
+                            background: url("../../assets/images/mainScreen/icon_zhxx_xz.png") no-repeat;
+                        }
                     }
-                    i {
-                        display: inline-block;
-                        width: 30px;
-                        height: 30px;
-                        margin-right: 5px;
-                        vertical-align: text-top;
+                    .active {
+                        background: url("../../assets/images/mainScreen/btn_xz.png") no-repeat;
+                        font-size: 26px;
+                        color: #e9f4ff;
                     }
-                    .nav-icon1 {
-                        background: url("../../assets/images/mainScreen/icon_zhxx.png") no-repeat;
-                    }
-                    .nav-icon1-active {
-                        width: 34px;
-                        height: 35px;
-                        background: url("../../assets/images/mainScreen/icon_zhxx_xz.png") no-repeat;
-                    }
-                    .nav-icon2 {
-                        background: url("../../assets/images/mainScreen/icon_jyjl.png") no-repeat;
-                    }
-                    .nav-icon2-active {
-                        width: 34px;
-                        height: 35px;
-                        background: url("../../assets/images/mainScreen/icon_zhxx_xz.png") no-repeat;
-                    }
-                    .nav-icon3 {
-                        background: url("../../assets/images/mainScreen/icon_dwzz.png") no-repeat;
-                    }
-                    .nav-icon3-active {
-                        width: 34px;
-                        height: 35px;
-                        background: url("../../assets/images/mainScreen/icon_zhxx_xz.png") no-repeat;
-                    }
-                    .nav-icon4 {
-                        background: url("../../assets/images/mainScreen/icon_yysc.png") no-repeat;
-                    }
-                    .nav-icon4-active {
-                        width: 34px;
-                        height: 35px;
-                        background: url("../../assets/images/mainScreen/icon_zhxx_xz.png") no-repeat;
-                    }
-                    .nav-icon5 {
-                        background: url("../../assets/images/mainScreen/icon_cjyy.png") no-repeat;
-                    }
-                    .nav-icon5-active {
-                        width: 34px;
-                        height: 35px;
-                        background: url("../../assets/images/mainScreen/icon_zhxx_xz.png") no-repeat;
-                    }
-                    .nav-icon6 {
-                        background: url("../../assets/images/mainScreen/icon_cjyy.png") no-repeat;
-                    }
-                    .nav-icon6-active {
-                        width: 34px;
-                        height: 35px;
-                        background: url("../../assets/images/mainScreen/icon_zhxx_xz.png") no-repeat;
-                    }
-                }
-                .active {
-                    background: url("../../assets/images/mainScreen/btn_xz.png") no-repeat;
-                    font-size: 26px;
-                    color: #e9f4ff;
                 }
             }
         }
@@ -223,7 +233,7 @@
                 align-items: center;
                 justify-content: flex-end;
 
-                .el-button{
+                .el-button {
                     height: 28px;
                     margin-right: 20px;
                     background: #403A6D;
